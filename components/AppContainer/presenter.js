68a933f27a3b8368bacf49de4c70c3dbc5fef62d
Seungheon Kim <roy@Seungheonui-MacBook-Pro.local>
2018-10-07 16:57:59 +0900

    Settings AppContainer && Redux ConfigureStore

---
+++ components/AppContainer/presenter.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types'
+import { View, Text, StatusBar, StyleSheet } from "react-native";
+
+class AppContainer extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoggedIn ? (
+          <Text>Logged In.</Text>
+        ) : (
+          <Text>Fuck Up</Text>
+        )}
+      </View>
+    )
+    
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
+
+export default AppContainer;
---
+++ redux/modules/user.js
@@ -0,0 +1,32 @@
+// Imports
+
+// Actions
+
+// Action Creators
+
+// API Actions
+
+// Initial State
+const initialState = {
+  isLoggedIn: false
+};
+
+// Reducer
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    // case typeName:
+    //   return { ...state };
+
+    default:
+      return state;
+  }
+}
+
+// Reducer Functions
+
+// Export
+
+// Default Reducer Export
+
+export default reducer;
