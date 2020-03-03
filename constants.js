3bbe2539883f055f9bbfe1d63afa5ad0c3f9f1f5
shkim <shkim.roy.d@gmail.com>
2020-03-03 14:40:10 +0900

    Change repository

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
-export const API_URL = "http://07cb3854.ngrok.io";
+// export const API_URL = "http://07cb3854.ngrok.io";
 // export const API_URL = "http://localhost:8000";
+export const API_URL = "http://instagram.ap-northeast-2.elasticbeanstalk.com";
 export const FB_APP_ID = "333729173869681";
+
